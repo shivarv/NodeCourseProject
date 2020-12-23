0f5618e7ad50857cf7c201e44e62d4f8985601db
shiva <shivarv1993@gmail.com>
2020-12-23 00:29:51 +0000

    commit basic node

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-const http = require("http");
+const express = require('express');
+const app = express();
 
-const routes = require("./routes");
-console.log(routes.someText);
-const server = http.createServer(routes.handler);
+app.use('/', (req, res, next) => {
+    console.log('This always runs!');
+    next();
+});
 
-server.listen(3000);
+app.use('/add-product', (req, res, next) => {
+    console.log('In the middleware');
+    res.send('<form action="/product" method="post"><input type="text" name="title"></input><button type="submit">add Product</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+    console.log('in product '+req.body);
+
+    res.redirect('/');
+});
+app.use('/', (req, res, next) => {
+    console.log('In the middleware');
+    res.send('<h1>Hello from Express!</h1>');
+});
+app.listen(3000);
