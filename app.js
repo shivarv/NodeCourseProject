7fbeb144681ea618dff6e432a508ba95ec4719cc
shiva <shivarv1993@gmail.com>
2020-12-27 20:52:10 +0000

    ejs complete

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const expressHbs = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop.js');
 //we have set the view engine as handlebars ... hbs must be the file extension
-app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
-app.set('view engine', 'hbs'); // app.set is to set global configuration item... eg ejs , pug, handlers for dynamic template
+//app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'})); // pug , ejs doesnt need this
+app.set('view engine', 'ejs'); // app.set is to set global configuration item... eg ejs , pug, handlers for dynamic template
 app.set('views', 'views'); // to tell nodejs that this is in views folder , default is same, so its generally not needed
 
 
